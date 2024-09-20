235a5bd9ae71b0c693b746764c9a74855f028eb5
zhaoxing03 <zhaoxing03@kuaishou.com>
2024-09-20 10:50:10 +0800

    beta

--- src/list/editors.tsx
+++ src/list/editors.tsx
@@ -1,33 +1,9 @@
-import { Layout } from './constants'
-
 export default {
   ':slot': {},
   '@init': ({ style }) => {
     style.height = 'auto';
   },
   '@resize': {
-    options: ['width', 'height']
+    options: ['width']
   },
-  ':root': {
-    items: [
-      {
-        title: '布局类型',
-        type: 'select',
-        description: '组件提供三种布局方式，包括横向、纵向、栅格布局',
-        options: [
-          { label: '横向布局', value: Layout.Horizontal },
-          { label: '纵向布局', value: Layout.Vertical },
-          { label: '栅格布局', value: Layout.Grid }
-        ],
-        value: {
-          get({ data }: any) {
-            return data.layout;
-          },
-          set({ data }: any, val: Layout) {
-            data.layout = val;
-          }
-        }
-      },
-    ]
-  }
 };
--- src/list/runtime.vue
+++ src/list/runtime.vue
@@ -1,17 +1,8 @@
 <template>
   <div class="listRoot">
-    <template v-if="props.data?.layout === 'vertical'">
-      <div v-for="(row, index) in listData">
-        <slot name="item" :inputValues="{ itemData: row, index: index }" :key="`row_${index}`"></slot>
-      </div>
-    </template>
-    <template v-else>
-      <div class="hor">
-        <div v-for="(row, index) in listData">
-          <slot name="item" :inputValues="{ itemData: row, index: index }" :key="`row_${index}`"></slot>
-        </div>
-      </div>
-    </template>
+    <div v-for="(row, index) in listData">
+      <slot name="item" :inputValues="{ itemData: row, index: index }" :key="`row_${index}`"></slot>
+    </div>
   </div>
 </template>
 
@@ -22,10 +13,9 @@ defineOptions({
   inheritAttrs: false,
 });
 
-const props = defineProps(['data', 'inputs', 'outputs', 'slots', 'env', 'style'])
+const props = defineProps(['data', 'inputs', 'outputs', 'slots', 'env', 'style']);
 
 const listData = ref(props?.env?.edit ? [1, 2, 3] : []);
-
 
 onMounted(() => {
   props.inputs['dataSource'](ds => {
@@ -41,9 +31,5 @@ onMounted(() => {
   min-height: 60px;
   height: inherit;
   width: inherit;
-}
-
-.hor {
-  display: inline-flex;
 }
 </style>
