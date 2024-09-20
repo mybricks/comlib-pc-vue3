c523fcbd7d09b7f3654f2e90fb10b7b2eb06b227
zhaoxing03 <zhaoxing03@kuaishou.com>
2024-09-20 10:36:05 +0800

    beta

--- src/tree/runtime.vue
+++ src/tree/runtime.vue
@@ -22,24 +22,29 @@ const placeholderTreeData = [
   {
     title: '0(搭建态占位数据)',
     key: '0',
+    iconUrl: "http://assets.mybricks.world/files/609813000904773/uMwh3xuQDcfoV4h17x69zFpuFV2ywmgO-1726798460278.png",
     children: [
       {
         title: '0-0',
         key: '0-0',
+        iconUrl: "http://assets.mybricks.world/files/609813000904773/uMwh3xuQDcfoV4h17x69zFpuFV2ywmgO-1726798460278.png",
         children: [
           {
             title: '0-0-1',
             key: '0-0-1',
+            iconUrl: "http://assets.mybricks.world/files/609813000904773/uMwh3xuQDcfoV4h17x69zFpuFV2ywmgO-1726798460278.png"
           },
           {
             title: '0-0-2',
             key: '0-0-2',
+            iconUrl: "http://assets.mybricks.world/files/609813000904773/uMwh3xuQDcfoV4h17x69zFpuFV2ywmgO-1726798460278.png"
           },
         ],
       },
       {
         title: '0-1',
         key: '0-1',
+        iconUrl: "http://assets.mybricks.world/files/609813000904773/uMwh3xuQDcfoV4h17x69zFpuFV2ywmgO-1726798460278.png"
       },
     ],
   }
@@ -53,14 +58,20 @@ onMounted(() => {
       ds = [];
     }
 
-    ds = ds.map(item => {
-      if (item.icon) {
-        item.iconUrl = item.icon;
-        delete item.icon;
-      }
+    // 递归 ds，将 icon 字段转换为 iconUrl
+    const recursive = (data) => {
+      data.forEach(item => {
+        if (item.icon) {
+          item.iconUrl = item.icon;
+          delete item.icon;
+        }
+        if (item.children) {
+          recursive(item.children);
+        }
+      });
+    }
 
-      return item;
-    })
+    recursive(ds);
 
     treeData.value = ds;
   })
